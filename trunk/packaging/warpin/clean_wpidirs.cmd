027d683ca30a7f0cce2c634ec07094814cf30de9
gyoung <gyoung@cadfd85e-12d1-dd11-ae90-00e081727c95>
2013-10-03 14:10:26 +0000

    Additional stuff that may be needed for warpin build

---
+++ trunk/packaging/warpin/clean_wpidirs.cmd
@@ -0,0 +1,29 @@
+/*
+ * $Id: clean_wpidirs.cmd 1246 2008-10-09 12:30:04Z jbs $
+ *
+ * REXX program to empty FM/2 Warpin WPI file staging directories
+ *
+ */
+
+call RxFuncAdd 'SysFileTree', 'REXXUTIL', 'SysFileTree'
+call SysFileTree 'PACKAGE.*', 'pkgdirs.', 'DO'
+do p = 1 to pkgdirs.0
+   call RemoveDir pkgdirs.p
+end
+return
+
+RemoveDir: procedure
+   parse arg dir
+   call SysFileTree dir'\*', 'files.', 'FO'
+   do i = 1 to files.0
+      '@attrib -r -s -h 'files.i' >NUL 2>NUL'
+      '@del 'files.i' >NUL 2>NUL'
+   end
+   drop files.
+   call SysFileTree dir'\*', 'dirs.', 'DO'
+   do i = 1 to dirs.0
+      call RemoveDir dirs.i
+   end
+   '@rd 'dir
+return
+
